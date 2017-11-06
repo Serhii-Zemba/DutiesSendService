72f4de1ac86ecbe8f9fcccfdc446db3cdc0b8778
Sergey Zemba <sergry.zemba@gmail.com>
2017-11-06 21:02:48 +0200

    Added code allowing to run only one instance of this app

--- DutiesSendService/Program.cs
+++ DutiesSendService/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net;
@@ -7,13 +8,20 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DutiesSendService
 {
     class Program
     {
+        private static Mutex mutex = new Mutex(true, Process.GetCurrentProcess().ProcessName);
         static void Main(string[] args)
         {
+            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                MessageBox.Show("Выдача нарядов уже запущена");
+                return;
+            }
 
             var notifyThread = new Thread(() =>
             {
